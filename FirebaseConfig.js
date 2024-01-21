e62e0c12e674c574cb25bbacaf3d8f37613fda3c
Renreal <renreal.dev@gmail.com>
2024-01-21 19:30:44 +0800

    uppercased changed

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,18 +1,6 @@
 
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
-   /*  // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyAgbOwN82nOnMm2TQtlAIEkObBbtaH9okw",
-      authDomain: "ticketforce-87c1d.firebaseapp.com",
-      projectId: "ticketforce-87c1d",
-      storageBucket: "ticketforce-87c1d.appspot.com",
-      messagingSenderId: "389530136369",
-      appId: "1:389530136369:web:2bf5bdcc84ebbc86474a10"
-    }; */
     const firebaseConfig = {
         apiKey: "AIzaSyAuH9VHIHMBwrTOaUg2MZau4qsCpbVFSn4",
         authDomain: "ticketforce-5ac87.firebaseapp.com",
